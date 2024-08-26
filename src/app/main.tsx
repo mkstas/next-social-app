213032e249136f1f0d2ae30d9132751cbb4e78e4
mkstas <makarov.stn@gmail.com>
2024-08-26 07:38:06 +0300

    change folders

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
-import './assets/index.css';
+import './index.css';
 
 /**
  * Import the generated route tree
  */
-import { routeTree } from './routeTree.gen';
+import { routeTree } from '../routeTree.gen';
 
 /**
  * Create a new router instance
