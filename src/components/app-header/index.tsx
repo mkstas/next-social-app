a0813a2cb477e3fa926c36a5e34dc1c99905efd6
mkstas <makarov.stn@gmail.com>
2024-08-26 07:42:40 +0300

    create component app-header

---
+++ src/components/app-header/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from '@tanstack/react-router';
+import { UserCircleIcon } from '@heroicons/react/24/outline';
+import { Container } from '@/shared/ui';
+
+export function AppHeader() {
+  return (
+    <header className='py-2'>
+      <Container className='flex items-center justify-between'>
+        <Link to='/' className='flex items-center space-x-2 text-lg font-semibold uppercase'>
+          <span className='py-1 px-2 text-white bg-violet-600 rounded-md'>React</span>
+          <span>Social</span>
+        </Link>
+        <div className='-mr-2'>
+          <Link to='/auth' className='flex items-center space-x-1 p-1 hover:text-violet-600'>
+            <UserCircleIcon className='w-6 h-6' />
+            <span className='text-sm font-semibold'>Войти</span>
+          </Link>
+        </div>
+      </Container>
+    </header>
+  );
+}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router';
-import { UserCircleIcon } from '@heroicons/react/24/outline';
-import { Container } from '@/shared/Container';
+import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { AppHeader } from '@/components/app-header';
 
 export const Route = createRootRoute({
   component: () => (
@@ -9,21 +8,10 @@ export const Route = createRootRoute({
        * Light theme: bg-white border-slate-200
        * Dark theme: bg-neutral-800 border-neutral-700
        */}
-      <header className='py-2'>
-        <Container className='flex items-center justify-between'>
-          <Link className='flex items-center space-x-2 text-lg font-semibold uppercase'>
-            <span className='py-1 px-2 text-white bg-violet-600 rounded-md'>React</span>
-            <span>Social</span>
-          </Link>
-          <div className='-mr-2'>
-            <Link className='flex items-center space-x-1 p-1 hover:text-violet-600' to='/auth'>
-              <UserCircleIcon className='w-6 h-6' />
-              <span className='text-sm font-semibold'>Войти</span>
-            </Link>
-          </div>
-        </Container>
-      </header>
-      <Outlet />
+      <AppHeader />
+      <main>
+        <Outlet />
+      </main>
     </>
   ),
 });
